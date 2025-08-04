728f7802f2b3a0ba396c85cef46dd8fb61d94d41
Shubham Thakre <shubhamthakre43@gmail.com>
2025-08-05 03:01:10 +0530

    Removed an unused elemet again

--- src/Components/ProfilePage/Profile.jsx
+++ src/Components/ProfilePage/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Profile.css';
 import { UserContext } from '../UserContext';
 import ImageCropper from '../CropperComponents/ImageCropper';
